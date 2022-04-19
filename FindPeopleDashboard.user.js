0381278b57190d2ea8f1534ce81a12222d8ca0cd
atehortg <103981973+atehortg@users.noreply.github.com>
2022-04-19 09:26:59 +0200

    Update FindPeopleDashboard.user.js

--- FindPeopleDashboard.user.js
+++ FindPeopleDashboard.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Find People Dash
-// @description  mUESTRA los AA que se encuentran en las zonas de picking en un esquema resumido, controla el IDLE y evita que estés refrescando la pantalla ya que lo hace automaticamente.
+// @description  Muestra los AA que se encuentran en las zonas de picking en un esquema resumido, controla el IDLE y evita que estés refrescando la pantalla ya que lo hace automaticamente.
 // @author       atehortg@
 // @match        https://aftlite-portal-eu.amazon.com/labor_tracking/find_people*
 // @updateURL    https://raw.githubusercontent.com/atehortg/scripts/main/FindPeopleDashboard.js
